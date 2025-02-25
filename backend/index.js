6f6e56e6d72fa1fc9081ec86e0f2c63ccc880ae7
Anish Kumar <kanish92729@gmail.com>
2025-02-25 20:56:10 +0530

    cors sec

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
-
+import cors from "cors"
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
@@ -21,7 +21,7 @@ mongoose
 
 const app = express()
 
-const cors = require('cors');
+
 app.use(cors({
   origin: ['*'],
   credentials: true
